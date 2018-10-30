b470e3238aa68adc37987aa3c0a87ec39689c10d
Klaus-poppe <vesparadkt@gmail.com>
2018-10-30 13:42:48 +0530

    Initial commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  courses ;
+  courses:any;
   prices ;
-  choice;
-  price ;
+  course ;
+  price;
 
   constructor() {
     this.courses = [ 'English', 'Math' , 'Science' ];
